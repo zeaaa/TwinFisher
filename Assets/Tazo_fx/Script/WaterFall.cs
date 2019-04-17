d5d67c483a1eb038f72e6264dc99ed8cabd93d35
zgxmy <zgxmy@126.com>
2019-04-17 21:30:50 +0800

    池塘场景更新
    -调整了池塘的渲染层级
    -瀑布脚本js改写为c#

---
+++ Assets/Tazo_fx/Script/WaterFall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFall : MonoBehaviour
+{
+    int targetMaterialSlot=0;
+    //var scrollThis:Material;
+    float speedY=0.5f;
+    float speedX=0.0f;
+    private float timeWentX=0;
+    private float timeWentY=0;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent< Renderer > ();
+        rend.materials[targetMaterialSlot].SetTextureOffset("_MainTex",new Vector2(timeWentX, timeWentY));
+       
+    }
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+        rend = GetComponent< Renderer > ();
+
+        rend.materials[targetMaterialSlot].SetTextureOffset("_MainTex", new Vector2(0, 0));
+        timeWentX = 0;
+        timeWentY = 0;
+    }
+
+    private void Update()
+    {
+        timeWentY += Time.deltaTime * speedY;
+        timeWentX += Time.deltaTime * speedX;
+        rend.materials[targetMaterialSlot].SetTextureOffset("_MainTex", new Vector2(timeWentX, timeWentY));
+    }
+}
